d86d5084d245c667cdbe1a0a2894cb8861cb16c9
forthlee <forthlee@gmail.com>
2025-05-08 08:01:11 +0800

    Update brainf.c

--- tests/brainf.c
+++ tests/brainf.c
@@ -5,12 +5,7 @@
 //    http://www.github.com/phimuemue
 
 // Usage:
-//   ./c4 tests/brainf.c "`cat tests/prime.bf`" 30000
-// First argument is code to run. Must be quoted.
-// Second argument is tape length. 30000 works for the prime number generator.
-
-// int abs(int n) { return n < 0 ? n * -1 : n; }
-// void putchar(char c) { printf("%c", c); }
+//   ./c4 tests/brainf.c prime.bf
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,41 +13,54 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(int a,char**s){
+int main(int a, char**s){
 	int *b, *z, p;
 	char *c, v, w;
+	int64_t fd, i, poolsz;
 
-	if(a < 3)
-		return printf("Usage: %s \"code\" length\n"
-		              "Where:\n"
-		              "   \"code\"   Quoted code to run. Try \"`cat file`\".\n"
-		              "    length  Tape length. Try 30000.\n", s[0]);
+	if(a < 2)
+		return printf("Usage: %s file.bf\n", s[0]);
 
-	// p = atoi(s[2])
-	p = 0; while(*s[2]) p = p * 10 + (*s[2]++ - '0');
-	if(!(b = z = malloc(p))) return printf("Failed to allocate %ld bytes\n", p);
-	c = s[1];
-
-	while(*c) {
-		p = 1;
-		*c-'>' || (++z);
-		*c-'<' || (--z);
-		*c-'+' || (++*z);
-		*c-'-' || (--*z);
-		*c-'.' || (printf("%c", *z));
-		*c-',' || (*z =getchar());
-		if(*c == '[' || *c == ']') {
-			v = *c;
-			w = 184 - v;
-			if(v < w ? *z == 0 : *z != 0)
-				while(p) {
-					c = c + (v < w ? 1 : -1);
-					p = p + (*c == v ? 1 : *c == w ? -1 : 0);
+	p = 30000;
+	if(!(b = z = malloc(p))) return printf("Failed to allocate %d bytes\n", p);
+	
+	if ((fd = open(s[1], 0)) < 0) { printf("could not open(%s)\n", s[1]); return -1; }
+	
+	poolsz = 256*1024;
+	if (!(c = malloc(poolsz))) { printf("could not malloc(%lld) source area\n", poolsz); return -1; }
+	if ((i = read(fd, c, poolsz-1)) <= 0) { printf("read() returned %lld\n", i); return -1; }
+	c[i] = 0;
+	close(fd);
+    
+    while(*c) {
+		if     (*c == '>') ++z;
+		else if(*c == '<') --z;
+		else if(*c == '+') ++*z;
+		else if(*c == '-') --*z;
+		else if(*c == '.') printf("%c", *z);
+		else if(*c == ',') *z = getchar();            
+		else if(*c == '[') {
+			if(!*z) { // 值為0，往前移至配對的']'位址
+				p = 1;                
+				while(p) { // 往前找配對的']'位址
+					c++; 
+					if(*c == '[') p++;
+					if(*c == ']') p--;
 				}
+			}
 		}
-		c++;
-	}
+		else if(*c == ']') {            
+			p = 1;
+			while(p) { // 往後移至配對的'['位址 
+				c--;             
+				if(*c == '[') p--;
+				if(*c == ']') p++;
+			}
+			c--;
+		}
+        c++;
+    }
 
-	free(b);
-	return 0;
+    free(b);
+    return 0;
 }
